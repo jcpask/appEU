8dd4716f6585dcbf432d435920c55e5c9c378e71
jcpask <c.paskewich@centre.edu>
2019-10-30 23:35:01 -0400

    added new stuff

--- data.js
+++ data.js
@@ -1,12 +1,91 @@
 
-//Data (JSON)
-//https://cohesiondata.ec.europa.eu/resource/pi4w-3vc9.json
-//EU Explanation page
-//https://cohesiondata.ec.europa.eu/2014-2020/2014-2020-Financial-Allocations-by-Member-State-Br/pi4w-3vc9
-//Wiki explanation of some funds
-//https://en.wikipedia.org/wiki/Structural_Funds_and_Cohesion_Fund
 
+/*PLAN: 
+1. Make promise.then function work (and try calling a dummy function in the .then too)
+2. Get header in place (without function for waiting if data loads)
+3. Draw Tables 
+3. Sort tables 
+4. Add css 
+*/
 
+var appPromise = d3.json("https://cohesiondata.ec.europa.eu/resource/pi4w-3vc9.json")
+appPromise.then(
+function(data)
+        {
+        //Put in here functions I need to call
+         makeTable(data); 
+        }) 
+
+//This function draws the table
+var makeTable = function(data)
+{
+    //d3.select("#mainTable").remove();
+     var rows = d3.select("#mainTable")
+    .selectAll("tr")
+    .data(data)
+    .enter()
+    .append("tr");
+    
+    //These 3 addCol functions provide data to the table
+    addCol(rows,function(data)
+    {
+        return data.member_state;
+    })
+    
+    addCol(rows,function(data)
+    {
+        return data.type;
+    }) 
+    
+    addCol(rows,function(data)
+    {
+        return data.eu_amount;
+    }) 
+     
+}
+
+//This is a function to add columns
+var addCol=function(rows, fcn)
+    {
+       rows.append("td").text(fcn);
+    }
+
+/*
+var sortColumn = function(data,col,accessor)
+{
+        d3.select(col)
+        .on("click",function()
+    {
+        data.sort(function(a,b) 
+        { 
+            return (accessor(a)-accessor(b));
+        })
+        makeTable(data);
+    }
+
+sortColumn(planets,"#density",function(p){return p.density});
+}
+*/
+
+var setButtons = function(data)
+{
+  d3.select("#basic").on("click",function(data)
+                        {
+        makeTable(data,"member_state")
+    });
+    
+    /*d3.select("#type").on("click",function()
+                        {
+        makeTable(planets,"Type")
+    });
+    
+    d3.select("#fund").on("click",function()
+                        {
+        makeTable(planets,"Fund")
+    });   */    
+}
+
+/*
 var sortColumn = function(data,col,accessor)
 {
     d3.select(col)
@@ -32,7 +111,8 @@ var makeTableHeader = function(data)
             if(a.MS > b.name ) { return 1; }
         }),"ALL")    
     })
-    
+*/
+/*
     sortColumn(planets,"#moons",function(p){return p.moons});
     sortColumn(planets,"#distance",function(p){return p.distance});
     sortColumn(planets,"#img",function(p){return p.distance});
@@ -59,6 +139,7 @@ var setButtons = function(data)
     });       
 }
 
+
 var makeTable = function(data,mode)
 {
     d3.selectAll("tbody *").remove();
@@ -69,7 +150,7 @@ var makeTable = function(data,mode)
     .enter()
     .append("tr");
     
-    
+  
     addCol(rows,function(data){return d."member-state"})
     
     rows.append("td")
@@ -137,21 +218,95 @@ var setBanner = function(message)
 {
     d3.select("#banner").text(message);
 }
+*/
 
 
-var appPromise = d3.json("https://cohesiondata.ec.europa.eu/resource/pi4w-3vc9.json")
+/*
+//I would like to create 2 columns of a table: the first displays the image of each penguin, the second displays the final score
 
+var appPromise = d3.json("/penguins/classdata.json");
 appPromise.then(
-function(data)
-{
-    setBanner("Here are the basic EU allocations");
-    makeTableHeader(data);
-    setButtons(data);
-    makeTable(data,"ALL");
-},
-function(err)
-{
-    setBanner("No data today, try again");
-});
+    function(penguins)
+        {
+          makeTable(penguins); 
+          getGrade(penguins);
+          getQuizMean(penguins);
+        }) 
+//brings my data in, and calls the function to draw the table.
 
 
+//This function draws my table
+var makeTable = function(penguins)
+{
+    //d3.select("#mainTable").remove();
+     var rows = d3.select("#mainTable")
+    .selectAll("tr")
+    .data(penguins)
+    .enter()
+    .append("tr");
+    
+    //this adds a column of images of penguins 
+    rows.append("td")
+        .append("img")
+        .attr("src",function(d)
+        {
+            return "penguins/" + d.picture;
+        //look at why "penguins" etc.
+        });
+
+    //These addCol functions are self-designed (not from the language), and take functions as inputs to basically automate the table generating process 
+    addCol(rows,function(penguin)
+    {
+        return penguin.final[0].grade;
+    })
+    
+    addCol(rows,function(penguin)
+    {
+        return getQuizMean(penguins);
+    }) 
+     
+}
+//We want this addCol to be its own function and automate what we did from 37-47
+//When building function like this, try leaving the inside of "function()" blank and figure it out as we move forward
+//We add "rows" as parameter because it doesn't know what the hell rows is otherwise
+var addCol=function(rows, fcn)
+    {
+       rows.append("td").text(fcn)
+    
+    }
+
+//These functions calculate the means and other stuff used below as inputs
+ var getGrade = function(penguins)
+ {
+    return penguins.quizes.grade;
+ }
+
+var getQuizMean = function(penguin)
+    {
+        return d3.mean(penguin.quizes,getGrade);
+    }
+
+//use this function addCol we created because it tells me what's going on, feels more intuitive, helps me remember; also, if our initial function had a small mistake and I copy/paste it a million times I have all those mistakes, just like adding CSS to everything is easier here too. REMEMBER: function can be packed up and used as a parameter; 
+
+//To add text to next column, a function is needed.
+    //Make functions when you realize you're doing too much work (maybe copy/paste a lot, etc.)
+    //at its core, d3 is taking the data and creating something with html to match up
+ 
+  /* rows.append("td").text(function(penguin)
+        {
+            return penguin.final[0].grade
+            //study syntax for lines 37/39
+        });
+    
+    //create new column of first quiz grade for each penguin
+    rows.append("td").text(function(penguin)
+        {
+        return penguin.quizes[0].grade
+    })*/
+
+//Data (JSON)
+//https://cohesiondata.ec.europa.eu/resource/pi4w-3vc9.json
+//EU Explanation page
+//https://cohesiondata.ec.europa.eu/2014-2020/2014-2020-Financial-Allocations-by-Member-State-Br/pi4w-3vc9
+//Wiki explanation of some funds
+//https://en.wikipedia.org/wiki/Structural_Funds_and_Cohesion_Fund
